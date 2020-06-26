1a4c98d60b034acb07ed71724cc7a2d2d57dfdb9
catl <l635743118@163.com>
2020-06-26 23:12:01 +0800

    charpter6 before  36 only used jdbctemplate

--- src/main/java/edu/snnu/css/DAODemo/DaoDemoApplication.java
+++ src/main/java/edu/snnu/css/DAODemo/DaoDemoApplication.java
@@ -1,12 +1,15 @@
 package edu.snnu.css.DAODemo;
 
 import edu.snnu.css.DAODemo.dao.StudentDAO;
+import edu.snnu.css.DAODemo.entity.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DaoDemoApplication implements CommandLineRunner {
@@ -22,6 +25,9 @@ public class DaoDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("查询所有学生信息——————>{}",studentDAO.findall());
+//		logger.info("查询所有学生信息——————>{}",studentDAO.findAll());
+//		logger.info("根据ID查询——————>{}",studentDAO.findById("41712146"));
+//		logger.info("根据ID查询——————>{}",studentDAO.deleteById("41712147"));
+		logger.info("插入——————>{}",studentDAO.insert(new Student("41712147","lialai","nanning", new Date( ))));
 	}
 }
--- src/main/java/edu/snnu/css/DAODemo/dao/StudentDAO.java
+++ src/main/java/edu/snnu/css/DAODemo/dao/StudentDAO.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -14,8 +15,38 @@ public class StudentDAO {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public List<Student> findall(){
-        jdbcTemplate.execute("use snnu_demo");
+    public List<Student> findAll(){
+    jdbcTemplate.execute("use snnu_demo");
         return jdbcTemplate.query("select * from student",new BeanPropertyRowMapper<Student>(Student.class));
     }
+
+//    query data base on ID
+    public Student findById(String id){
+        jdbcTemplate.execute("use snnu_demo");
+        return jdbcTemplate.queryForObject("select * from student where id =? ",new Object[]{id} ,new BeanPropertyRowMapper<Student>(Student.class));
+    }
+
+    public int deleteById(String id){
+        jdbcTemplate.execute("use snnu_demo");
+        return jdbcTemplate.update("delete from student where id =? ",new Object[]{id} );
+    }
+
+    public int insert(Student student){
+        jdbcTemplate.execute("use snnu_demo");
+        return jdbcTemplate.update("INSERT INTO student (id,name,location,birth_date) VALUES (?,?,?,?)",
+                student.getId(),
+                student.getName(),
+                student.getLocation(),
+                new Timestamp(student.getBirthDate().getTime()));
+    }
+
+        public int updateById(Student student){
+        jdbcTemplate.execute("use snnu_demo");
+        return jdbcTemplate.update("update student SET name=?,location=?,birth_date=? ",
+                student.getId(),
+                student.getName(),
+                student.getLocation(),
+                new Timestamp(student.getBirthDate().getTime()));
+    }
+
 }
